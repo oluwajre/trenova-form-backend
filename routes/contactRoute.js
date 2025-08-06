ac5b1777e9c7a6e0d23e171493722b7106125700
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2025-08-06 23:06:23 +0100

    updated contactRoute.js to dynamically take in email address

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const router = express.Router();
 
 router.post('/contact/send-message', async (req, res) => {
-    const { name, email, message, subject, phone } = req.body;
+    const { name, email, message, subject, phone, receiver } = req.body;
 
     if (!name || !email || !message) {
         return res.status(400).json({ success: false, message: 'Name, email, and message are required.' });
@@ -19,7 +19,7 @@ router.post('/contact/send-message', async (req, res) => {
 
     const mailOptions = {
         from: email,
-        to: process.env.RECEIVER_EMAIL_TRINITWIN,
+        to: receiver,
         subject: subject || 'New Contact Form Message',
         text: `${name} has contacted you from your Trinitwin website contact form, below are the details of the Form:\n\nName: ${name}\nEmail: ${email}\nPhone Number: ${phone || 'Phone Number not provided'}\n\nMessage:\n${message}\n\nKindly reply ${name}.`,
     };
